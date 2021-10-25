aafb718a9314c31d43743294b6312300080a806f
vietct513 <89295573+vietct513@users.noreply.github.com>
2021-10-25 17:22:31 +0700

    add file btvn buoi 1

---
+++ bai tap ve nha/btvnbuoi1/giaibaitap/giaibai1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int A;
+    cout << "Nhap gia tri vao day: ";
+    cin >> A;
+    cout << A << endl;
+    system ("pause"); // tru 1 diem 
+    return 0;
+}
+stateging are => 
